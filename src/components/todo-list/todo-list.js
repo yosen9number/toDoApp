8ddc98392c965a6bb9825646fc872209cc9ba684
Наира Лалаян <nlalayan@ivi.ru>
2020-02-28 01:07:50 +0300

    create toDoApp

---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import ToDoListItem from "../todo-list-item";
+import './todo-list.css';
+
+const ToDoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+
+        const { id, ...itemProps } = item;
+
+       return (
+           <li key={ id } className='list-group-item'>
+               <ToDoListItem {...itemProps} />
+           </li>
+       );
+    });
+
+    return (
+        <ul className='list-group todo-list'>
+            { elements }
+        </ul>
+    );
+};
+
+export default ToDoList;
